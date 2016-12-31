36e44a9d3518f1752e2488e2b0fe02e9503c8bf9
Xavier Francisco <xavier.n.francisco@gmail.com>
2016-12-31 16:03:05 +0000

    fix

--- src/swing/MenuGrupo.java
+++ src/swing/MenuGrupo.java
@@ -253,6 +253,7 @@ public class MenuGrupo extends javax.swing.JFrame {
 
     private void jAddmembroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jAddmembroActionPerformed
         new AdicionarMembro(this,true,this.k,grupo).setVisible(true);
+        updateList();
     }//GEN-LAST:event_jAddmembroActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
